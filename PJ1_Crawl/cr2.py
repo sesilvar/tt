60b447c7853672e15d0d4727c3c88d1e50787e0c
sesilvar <89985531@qq.com>
2017-10-13 17:13:09 +0800

    Update cr2.py

--- PJ1_Crawl/cr2.py
+++ PJ1_Crawl/cr2.py
@@ -14,12 +14,13 @@ random.seed(datetime.datetime.now())
 
 def getlinks(articleurl):
     html = urlopen('http://en.wikipedia.org' + articleurl)
-    bs1 = BeautifulSoup(html)
-    return bs1.find('div', {'id': 'bodyContent'}).findall('a', href=re.compile('^(/wiki/)((?!:).) *$'))
+    bs1 = BeautifulSoup(html, 'html.parser')
+    return bs1.find('div', {'id': 'bodyContent'}).findAll('a', href=re.compile('^(/wiki/)((?!:).)*$'))
 
 
 links = getlinks('/wiki/Kevin_Bacon')
-while len(links) > 0:
+
+if len(links) > 0:
     newarticle = links[random.randint(0, len(links)-1)].attrs['href']
     print newarticle
-    links = getlinks(newarticle)
+#    links = getlinks(newarticle)
