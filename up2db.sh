d54623389c1681fb3c37f1a33588541e81e552bd
sesilvar <89985531@qq.com>
2017-12-20 21:31:11 +0800

    Create up2db.sh

---
+++ up2db.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cpu_load5=`uptime|awk '{print $9}'|sed 's/,//'`
+echo 'cpu_load_5:' $cpu_load5
+mem_free=`free -m|grep Mem|awk '{print $NF}'`
+echo 'mem_free:' $mem_free
+disk_free=`df -hm /|grep sda| awk '{print $4}'`
+echo 'disk_free:' $disk_free
+time_now=`date '+%Y-%m-%d %H:%M:%S'`
+echo 'time_now:' $time_now
+
+sql="insert into tt.up(cpu_load5,mem_free,disk_free,collect_time) value($cpu_load5, $mem_free, $disk_free, '$time_now');"
+echo $sql
+
+mysql -uroot -ppasswd -e "$sql"
+
+
+CREATE TABLE `up` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cpu_load5` decimal(5,2) DEFAULT NULL,
+  `mem_free` bigint(20) DEFAULT NULL,
+  `disk_free` bigint(20) DEFAULT NULL,
+  `collect_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+  );
+  
