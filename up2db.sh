ebee1a10947a1c04721ed824b701afeb3bd4afcd
sesilvar <89985531@qq.com>
2018-03-26 18:03:35 +0800

    Update up2db.sh

--- up2db.sh
+++ up2db.sh
@@ -1,26 +1,36 @@
-#!/bin/bash
-
-cpu_load5=`uptime|awk '{print $9}'|sed 's/,//'`
-echo 'cpu_load_5:' $cpu_load5
-mem_free=`free -m|grep Mem|awk '{print $NF}'`
-echo 'mem_free:' $mem_free
-disk_free=`df -hm /|grep sda| awk '{print $4}'`
-echo 'disk_free:' $disk_free
-time_now=`date '+%Y-%m-%d %H:%M:%S'`
-echo 'time_now:' $time_now
-
-sql="insert into tt.upload(cpu_load5,mem_free,disk_free,collect_time) value($cpu_load5, $mem_free, $disk_free, '$time_now');"
-echo $sql
-
-mysql -uroot -ppasswd -e "$sql"
+#!/usr/bin/env python
 
 
-CREATE TABLE `upload` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `cpu_load5` decimal(5,2) DEFAULT NULL,
-  `mem_free` int(20) unsigned DEFAULT NULL,
-  `disk_free` int(20) unsigned DEFAULT NULL,
-  `collect_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-  );
-  
+import xlwt
+import time
+import pymysql
+
+today = time
+item_name=['Processor load (15 min average per core)', 'Total memory', 'Available memory', 'Free inodes on $1 (percentage)', 'Free disk space on $1 (percentage)']
+
+def zabbix_report(day=today):
+	ip_list = ['10.10.8.202']
+	book = xlwt.Workbook()
+	sheet1 = book.add_sheet(day)
+	for ip in ip_list:
+		data = monitor_data(ip)
+	row = 1
+	col = 1
+	for 
+
+def monitor_data(ipaddr):
+	item_name=['Processor load (15 min average per core)', 'Total memory', 'Available memory', 'Free inodes on $1 (percentage)', 'Free disk space on $1 (percentage)']
+	sql_hostid = 'select hostid from interface where ip = %s;' %ipaddr
+	conn = pymysql.connect(host='10.10.8.251', port=3306, user='zabbix', passwd='DC@72mon', db='zabbix')
+	cursor = conn.cursor()
+	cursor.execute(sql_hostid)
+	hostid = cursor.fetchone()
+	for items in item_name:
+		sql_itemid = 'select itemid from items where hostid = %s and name = %s;' %(hostid,items)
+		cursor.execute(sql_itemid)
+		itemid=cursor.fetchall()
+		for itemid in:
+			sql_itemvalue = 'select * from trends where itemid='24617' and clock=(select max(clock) from trends where itemid=);'
+			
+if __name__='__main__':
+	main()
